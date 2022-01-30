184df69eb0337d0cbd3cdac88aa88c65d72c852c
josh <josch>
2022-01-30 20:25:17 +0100

    movingFolksAround

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -11,13 +11,20 @@ class Prospect: Identifiable, Codable {
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
+    
 }
 
 @MainActor class Prospects: ObservableObject {
     @Published var people: [Prospect]
-
+   
     init() {
         self.people = []
     }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+    }
+
 }
