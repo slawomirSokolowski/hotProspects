868f9c93045a3f4b276efbc9a14d86293a78bf02
josh <josch>
2022-01-30 18:21:44 +0100

    ScanQRcodes

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ProspectsView: View {
+    @State private var isShowingScanner = false
     @EnvironmentObject var prospects: Prospects
     
     enum FilterType {
@@ -52,14 +54,30 @@ struct ProspectsView: View {
                 .navigationTitle(title)
                 .toolbar {
                     Button {
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress = "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
+                        isShowingScanner = true
                     } label: {
                         Label("Scan", systemImage: "qrcode.viewfinder")
                     }
                 }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+                }
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
